cc4d9faacf68d841b8ecbfc7b1317967ad4a9cd7
Lee Sanderson <lee.sndrsn@gmail.com>
2023-01-04 17:52:08 +0000

    Added react router

---
+++ src/katas/src/Calculator.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface ICalculatorProps {}
+
+export function Calculator(props: ICalculatorProps) {
+  return <div>Hello</div>;
+}
--- src/katas/src/index.tsx
+++ src/katas/src/index.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Calculator } from "./Calculator";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    //loader: rootLoader,
+    /*children: [
+      {
+        path: "team",
+        element: <Team />,
+        loader: teamLoader,
+      },
+    ],*/
+  },
+  {
+    path: "/Calculator",
+    element: <Calculator />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
